a0179cdf311cada5ccc08a909aad946963c9ae17
Huy Pham <huyphamnguyen@gmail.com>
2022-09-16 17:13:43 +0700

    Update code

--- data/swap.js
+++ data/swap.js
@@ -2,10 +2,10 @@ import { BTC, STSol, ETH, Usdc, Usdt, Sol, Rin, Larix, Ray, Port, Solend, Orca }
 
 export const dataSwap = [
   { name: 'BTC', icon: <BTC />, id: 'bitcoin', desc: 'Wrapped Bitcoin'},
-  { name: 'stSol', icon: <STSol />, id: 'lido-staked-sol', desc: 'Lido Staked SOL'},
-  { name: 'ETH', icon: <ETH />, id: 'ethereum-wormhole' },
   { name: 'USDC', icon: <Usdc />, id: 'usd-coin-avalanche-bridged-usdc-e', desc: 'USD Coin'},
   { name: 'USDT', icon: <Usdt />, id: 'tether', desc: 'USDT'},
+  { name: 'stSol', icon: <STSol />, id: 'lido-staked-sol', desc: 'Lido Staked SOL'},
+  { name: 'ETH', icon: <ETH />, id: 'ethereum-wormhole' },  
   { name: 'SOL', icon: <Sol />, id: 'wrapped-solana', desc: 'Wrapped SOL'},
   { name: 'RIN', icon: <Rin />, id: 'aldrin', desc: 'Aldrin'},
   { name: 'mSol', icon: <img src='./images/mSol.png' />, id: 'msol', desc: 'Marinade staked SOL (mSOL)'},
